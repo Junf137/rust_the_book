90ded75533cf1a5d04510e960379837f4cfa2081
eric <3086638517@qq.com>
2023-12-31 11:29:04 +0800

    [functions] add proj

---
+++ functions/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+
+    another_function(i32::MAX, '🥺');
+}
+
+fn another_function(x: i32, ch: char) {
+    println!("x = {x}");
+    println!("ch = {ch}");
+}
